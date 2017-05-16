305f44da4bcb2de3be40afaebfee46815a253027
özenç çelik <zen elik>
2017-05-16 23:06:13 +0300

    Tasarımsal Değişiklikleri Yap

---
+++ app/src/main/java/com/android/zen/mobilehealthmonitor/PhysicalActivity.java
@@ -0,0 +1,36 @@
+package com.android.zen.mobilehealthmonitor;
+
+/**
+ * Created by ASUS-PC on 16.5.2017.
+ */
+
+public class PhysicalActivity {
+
+    private boolean isActive;
+    private String activityName;
+    private int DECREASE_SECONDS;
+
+    public PhysicalActivity(String name,int decreaseSecond){
+        isActive = false;
+        activityName = name;
+        DECREASE_SECONDS = decreaseSecond;
+    }
+
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public String getActivityName() {
+        return activityName;
+    }
+
+    public int getDecreaseSeconds() {
+        return DECREASE_SECONDS;
+    }
+
+}
